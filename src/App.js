8df49a9eabe8ace3b591f4694cba1f5204f1aa07
al3des <alejandrodesalvo@gmail.com>
2021-01-31 13:49:27 +0100

    added MessagesProvider to the root App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { CssBaseline, makeStyles, Paper } from "@material-ui/core"
 import Conversation from "./containers/Conversation"
 import SideBar from "./containers/SideBar"
+import MessagesProvider from "./context/MessagesContext"
 
 let useStyles = makeStyles({
   root: {
     height: "100vh",
     background: "pink",
     display: "grid",
-    gridTemplateColumns: "minmax(250px, 1fr) 3fr",
+    gridTemplateColumns: "minmax(300px, 1fr) 3fr",
     gridTemplateRows: "repeat(3, 1fr)",
   },
 })
@@ -15,11 +16,13 @@ let useStyles = makeStyles({
 function App() {
   let classes = useStyles()
   return (
-    <Paper className={classes.root}>
-      <CssBaseline />
-      <SideBar />
-      <Conversation />
-    </Paper>
+    <MessagesProvider>
+      <Paper className={classes.root}>
+        <CssBaseline />
+        <SideBar />
+        <Conversation />
+      </Paper>
+    </MessagesProvider>
   )
 }
 
