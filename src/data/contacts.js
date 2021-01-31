4afb8647eb5d573fe2e480f04d1766fceab9d1b5
al3des <alejandrodesalvo@gmail.com>
2021-01-31 13:46:50 +0100

    added generateNewContact() function

--- src/data/contacts.js
+++ src/data/contacts.js
@@ -4,10 +4,15 @@ const contacts = Array(20)
   .fill()
   .map((_, i) => {
     let name = superheroes.random()
-    return {
-      id: i + 1,
-      name,
-      avatarUrl: `https://avatars.dicebear.com/4.5/api/bottts/:${name}.svg`,
-    }
+    return generateNewContact(name, i)
   })
 export default contacts
+
+export function generateNewContact(name, i) {
+  return {
+    id: i + 1,
+    name,
+    avatarUrl: `https://avatars.dicebear.com/4.5/api/bottts/:${name}.svg`,
+    phoneNumber: Math.floor(Math.random() * 10),
+  }
+}
